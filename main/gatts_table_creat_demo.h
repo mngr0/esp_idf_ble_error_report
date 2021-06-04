c437568fd6739589ef59362e9fe3d25afb337aeb
mngr0 <marco.negrini6@studio.unibo.it>
2021-06-04 16:46:15 +0200

    initial commit, with example

---
+++ main/gatts_table_creat_demo.h
@@ -0,0 +1,38 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Attributes State Machine */
+enum
+{
+    IDX_SVC,
+    IDX_CHAR_A,
+    IDX_CHAR_VAL_A,
+   // IDX_CHAR_CFG_A,
+
+    IDX_CHAR_B,
+    IDX_CHAR_VAL_B,
+
+    IDX_CHAR_C,
+    IDX_CHAR_VAL_C,
+
+    IDX_CHAR_D,
+    IDX_CHAR_VAL_D,
+    HRS_IDX_NB,
+};
+
+
+#define BLINK_GPIO1 2
+#define BLINK_GPIO2 15
+#define BLINK_GPIO3 13
+#define BTN_GPIO 3
